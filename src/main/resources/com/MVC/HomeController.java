bce62c3ee80328a9f17080a605ae2b622894cd5c
Arvind337 <arvindmalipatil01@gmail.com>
2021-07-30 18:31:26 +0530

    Hi arvind for git local

---
+++ src/main/resources/com/MVC/HomeController.java
@@ -0,0 +1,14 @@
+package com.MVC;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class HomeController {
+
+	
+	public String home()
+	{
+		System.out.println("Hi home");
+	return index;
+	}
+}
